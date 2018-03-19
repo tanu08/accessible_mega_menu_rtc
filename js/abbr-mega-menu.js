99b2bb9b8c77f8489fedfa23da09c1c621bbb779
Tanu <tanurampal08@gmail.com>
2018-03-20 00:23:12 +0530

    Fixed scrollbar issue on smaller screen sizes when clicking another menu link with previous panel already opened

--- js/abbr-mega-menu.js
+++ js/abbr-mega-menu.js
@@ -274,6 +274,14 @@ limitations under the License.
                             "width" : $('.' + settings.menuClass).outerWidth()
                     });
                     $('.' + settings.menuClass).css('margin-bottom', topli.children('.' + settings.panelClass).outerHeight());
+
+                    var pageScrollPosition = $('html')[0].scrollTop;
+                    var openPanelTopPosition = $('.' + settings.panelClass + '.' + settings.openClass).parent().offset().top;
+
+                    if(pageScrollPosition > openPanelTopPosition) {
+                        $('html')[0].scrollTop = openPanelTopPosition;
+                    }
+
                 if (event.type === 'mouseover' && target.is(':tabbable') && topli.length === 1 && panel.length === 0 && menu.has(document.activeElement).length > 0) {
                     target.focus();
                     that.justFocused = false;
